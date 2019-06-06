d4dd50cf1817b8a0efb000f7ae6939b726960697
abarnaramaraj <47477481+abarnaramaraj@users.noreply.github.com>
2019-06-06 16:25:08 +0530

    Create Power5.java

---
+++ Power5.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+public class Power5
+{
+public static void main(String[] args)
+{
+int a,s,d,f;
+Scanner sc=new Scanner(System.in);
+a=sc.nextInt();
+s=sc.nextInt();
+d=sc.nextInt();
+f=(a*s)%d;
+System.out.println(f);
+}
+}
